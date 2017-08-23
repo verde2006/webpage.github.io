dce1db1447ec47b1dfc3ef2d28f455c8e9b398af
Tamer Khraisha <tamer.khraisha2@gmail.com>
2017-08-23 08:00:06 +0200

    Update application.js

--- application.js
+++ application.js
@@ -90,18 +90,6 @@ $(document).ready(function() {
   getHappyDay();
 });
 
-function getHappyDay() {
-  d = new Date();
-  weekdays = new Array(7);
-  weekdays[0] = "Sunday";
-  weekdays[1] = "Monday";
-  weekdays[2] = "Tuesday";
-  weekdays[3] = "Wednesday";
-  weekdays[4] = "Thursday";
-  weekdays[5] = "Friday";
-  weekdays[6] = "Saturday";
-  $("footer .day").html(weekdays[d.getDay()]);
-}
 
 function getAge() {
   var birthday = new Date("02 22, 1993");
@@ -112,10 +100,13 @@ function getAge() {
 
 // List of all phrases
 var Phrases = new Array(
-  "I go by the<br />name <a href='http://www.tamerkhraisha.com'>Tamer</a>",
-  "I am a Ph.D <br />at <a href='https://cns.ceu.edu/' target='_blank'>CNS</a>",
-  "I code stuff<br />on <a href='https://github.com/TamerKhraisha' target='_blank'>Github</a>",
-  "Follow me<br />on <a href='https://twitter.com/Tamer_Khraisha' target='_blank'>Twitter</a>",
+  "I go by the<br />name <a href='/about/'>Tamer</a>",
+  "I go to work<br />at <a href='http://www.opoloo.com' target='_blank'>Opoloo</a>",
+  "I push to master<br />on <a href='http://www.github.com/MyXoToD' target='_blank'>Github</a>",
+  "I code stuff<br />on <a href='http://www.codepen.io/MyXoToD' target='_blank'>CodePen</a>",
+  "I post tweets<br />on <a href='http://www.twitter.com/MyXoToD' target='_blank'>Twitter</a>",
+  //"I write stories<br />on <a href='https://medium.com/@MyXoToD' target='_blank'>Medium</a>",
+  "I listen to music<br />on <a href='http://open.spotify.com/user/1145610126' target='_blank'>Spotify</a>"
 );
 
 // Phases settings
@@ -172,15 +163,3 @@ function initPhrases() {
   }
 }
 
-april = {
-  fool: function() {
-    d = new Date();
-    month = d.getUTCMonth() + 1;
-    day = d.getUTCDate();
-    if (month == 4 && day == 1) {
-      if (window.location.href != "http://harlemify.com/?url=myxotod.com") {
-        window.location.href = "http://harlemify.com/?url=myxotod.com";
-      }
-    }
-  }
-};
